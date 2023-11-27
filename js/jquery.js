994753fda082c9278f38b107474910e4a8fd613a
pedroalexandrefs <pedroalexandrea13@gmail.com>
2023-11-27 16:27:17 -0300

    update

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
-  $("#cep").blur(function () {
-    var valor = $(this).val();
-    $.get(
-      "https://viacep.com.br/ws/" + valor + "/json/",
-      function (dados, status) {
-        $("#estado").val(dados.uf);
-        $("#cidade").val(dados.localidade);
-        $("#bairro").val(dados.bairro);
-        $("#rua").val(dados.logradouro);
-      }
-    );
+
+    $("#pesquisa").click(function () {
+      var valor = $("#cep").val();
+      $.get(
+        "https://viacep.com.br/ws/" + valor + "/json/",
+        function (dados, status) {
+          $("#estado").val(dados.uf);
+          $("#cidade").val(dados.localidade);
+          $("#bairro").val(dados.bairro);
+          $("#rua").val(dados.logradouro);
+        }
+      );
+    })
+    
   });
-});
+
